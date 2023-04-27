37bc7240772c7c91d45be5c78b9bd2a09fa7e48c
LTibo <lanyitibike1999@gmail.com>
2023-04-27 19:04:54 +0200

    register and login frontend

--- angular/PRF/src/app/app-routing.module.ts
+++ angular/PRF/src/app/app-routing.module.ts
@@ -6,11 +6,14 @@ import { ErrorComponent } from './error/error.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { WeatherComponent } from './weather/weather.component';
 import { ForecastComponent } from './forecast/forecast.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'weather', pathMatch: 'full'},
   {path: 'weather', component: WeatherComponent},
   { path: 'forecast/:city', component: ForecastComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegistrationComponent },
   {path: '**', component: ErrorComponent}
 ];
 
